b4c22bde4405b8b7990523f688d67cd9ad9844a8
mbalderasuaq <mbalderas08@alumnos.uaq.mx>
2025-03-22 17:03:01 -0600

    feat: add jwt retrieved

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,16 +1,50 @@
+"use client";
+
 import Link from "next/link";
 import { Input, Button } from "@nextui-org/react";
+import axios from "axios";
+import { API_URL } from "@/constants";
 
 export default function LoginPage() {
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    let authData: any = {};
+    authData.userEmail = formData.get("userEmail");
+    authData.userPassword = formData.get("userPassword");
+    const { data } = await axios.post(`${API_URL}/auth/login`, {
+      ...authData,
+    });
+    console.log(data);
+    return;
+  };
+
   return (
-    <div className="bg-orange-500 px-10 py-2 rounded-md">
+    <form
+      className="bg-orange-500 px-10 py-2 rounded-md"
+      onSubmit={handleSubmit}
+    >
       <p className="text-2xl my-4 text-white">Iniciar sesión</p>
       <div className="flex flex-col gap-2 my-4 items-center">
-        <Input label="Email" type="email" isRequired={true} size="sm" />
-        <Input label="Contraseña" type="password" isRequired={true} size="sm" />
+        <Input
+          label="Email"
+          name="userEmail"
+          type="email"
+          isRequired={true}
+          size="sm"
+        />
+        <Input
+          label="Contraseña"
+          name="userPassword"
+          type="password"
+          isRequired={true}
+          size="sm"
+        />
       </div>
       <div className="flex flex-col items-center gap-2">
-        <Button color="primary">Registrarse</Button>
+        <Button color="primary" type="submit">
+          Iniciar Sesión
+        </Button>
         <p className="text-white">
           ¿No tienes cuenta?{" "}
           <Link href="/signup" className="text-red-600 underline">
@@ -18,6 +52,6 @@ export default function LoginPage() {
           </Link>
         </p>
       </div>
-    </div>
+    </form>
   );
 }
